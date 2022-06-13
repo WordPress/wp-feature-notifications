05b51af94df1b316ee2a446456a51ec2e783397f
Erik Golinelli <erik@codekraft.it>
2022-06-13 20:17:34 +0200

    init storybook

---
+++ src/stories/Button.stories.jsx
@@ -0,0 +1,80 @@
+import React from '@wordpress/element';
+import '@wordpress/block-library/build-style/style.css';
+import '@wordpress/block-library/build-style/editor.css';
+import '@wordpress/block-library/build-style/theme.css';
+import '@wordpress/components/build-style/style.css';
+
+/**
+ * WordPress dependencies
+ */
+import {
+	more,
+} from '@wordpress/icons';
+
+import { Button } from '@wordpress/components';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+	title: 'Example/Button',
+	component: Button,
+	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = ( args ) => (
+	<Button { ...args }>{ args.label || 'clickme' }</Button>
+);
+
+export const Primary = Template.bind( {} );
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+	variant: 'primary', // The accepted values are 'primary' (the primary button styles), 'secondary' (the default button styles), 'tertiary' (the text-based button styles), and 'link' (the link button styles).
+	label: 'Button',
+};
+
+export const pressed = () => {
+	const label = 'isPressed';
+	return <Button isPressed>{ label }</Button>;
+};
+
+export const buttons = () => {
+	return (
+		<div style={ { padding: '20px' } }>
+			<h2>Small Buttons</h2>
+			<div className="story-buttons-container">
+				<Button isSmall>Button</Button>
+				<Button variant="primary" isSmall>
+					Primary Button
+				</Button>
+				<Button variant="secondary" isSmall>
+					Secondary Button
+				</Button>
+				<Button variant="tertiary" isSmall>
+					Tertiary Button
+				</Button>
+				<Button isSmall icon={ more } />
+				<Button isSmall variant="primary" icon={ more } />
+				<Button isSmall variant="secondary" icon={ more } />
+				<Button isSmall variant="tertiary" icon={ more } />
+				<Button isSmall variant="primary" icon={ more }>
+					Icon & Text
+				</Button>
+			</div>
+
+			<h2>Regular Buttons</h2>
+			<div className="story-buttons-container">
+				<Button>Button</Button>
+				<Button variant="primary">Primary Button</Button>
+				<Button variant="secondary">Secondary Button</Button>
+				<Button variant="tertiary">Tertiary Button</Button>
+				<Button icon={ more } />
+				<Button variant="primary" icon={ more } />
+				<Button variant="secondary" icon={ more } />
+				<Button variant="tertiary" icon={ more } />
+				<Button variant="primary" icon={ more }>
+					Icon & Text
+				</Button>
+			</div>
+		</div>
+	);
+};
---
+++ src/stories/Notification.stories.jsx
@@ -0,0 +1,42 @@
+import React from '@wordpress/element';
+import Notice from '../scripts/components/notification';
+import '../../build/wp-notify.css';
+
+export default {
+	title: 'Notification/Dashboard',
+	component: Notice,
+	parameters: {
+		backgrounds: {
+			default: 'WordPress',
+			values: [ { name: 'WordPress', value: '#f0f0f1' } ],
+		},
+	},
+};
+
+/**
+ * Single Notification UI component
+ * https://github.com/WordPress/wp-feature-notifications/issues/16#issuecomment-896031592
+ * https://github.com/WordPress/wp-feature-notifications/issues/37#issuecomment-896080025
+ */
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = ( args ) => (
+	<div id="wpbody">
+		<div id="wp-notify-dashboard-notices" className="wrap">
+			<Notice { ...args } />
+		</div>
+	</div>
+);
+
+export const Simple = Template.bind( {} );
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Simple.args = {
+	title: 'Notice Example',
+	message: 'ciao',
+	location: 'dashboard',
+	acceptMessage: 'OK',
+	acceptLink: '',
+	dismissLabel: 'bye',
+	dismissible: true,
+	date: Date.now() / 1000,
+};
