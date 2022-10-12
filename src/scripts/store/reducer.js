c3f730ab3ab54c777da302151ba104560bfe4263
Erik Golinelli <erik@codekraft.it>
2022-10-12 16:49:59 +0200

    updates the js api - add list and find List - It returns the notifications from the Redux store a search term Find - It searches the Redux store for a notification by ID or by a search term

--- src/scripts/store/reducer.js
+++ src/scripts/store/reducer.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { store } from '../wp-notify';
+import { __ } from '@wordpress/i18n';
 
 // TODO: maybe a filter is needed in order to add some custom places?
 const locations = ['dashboard', 'adminbar'];
@@ -42,4 +44,55 @@ const notifyController = createSlice({
 /** Exporting the actions and the reducer */
 export const { addNotice, removeNotice, clearNotices } =
 	notifyController.actions;
+
+/**
+ * It returns the notifications from the Redux store
+ *
+ * @param {string|false} location
+ *
+ * @return {store} The notifications state from the store.
+ */
+export const listNotices = (location = false) => {
+	if (location) {
+		const storeData = store.getState().notifications[location];
+		if (storeData) return storeData;
+	}
+	return store.getState().notifications;
+};
+
+/**
+ * It searches the Redux store for a notification by ID or by a search term
+ *
+ * @param {string} searchTerm - The term you want to search for.
+ * @param {Object} [args]     - search args
+ * @return {Object} the search result
+ */
+export const findNotice = (
+	searchTerm,
+	args = { key: 'source', location: 'dashboard' }
+) => {
+	// get the notifications array
+	const storeData = store.getState().notifications;
+
+	// return the notification by id and location
+	if (typeof searchTerm === 'number' && args.location) {
+		return storeData[args.location][searchTerm];
+	}
+
+	// search the notification by key and searchTerm
+	const found = [];
+	Object.entries(storeData).forEach((locationNotices) => {
+		const location = locationNotices[0];
+		found[location] = locationNotices[1].filter(
+			(el) =>
+				el[args.key] &&
+				el[args.key].toLowerCase().includes(searchTerm) && {
+					location: el,
+				}
+		);
+	});
+
+	return found ? found : __('Nothing was found');
+};
+
 export default notifyController.reducer;
