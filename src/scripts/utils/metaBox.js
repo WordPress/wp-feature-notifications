c44190e7955d9426e51ab2868f84b4fefda27226
Erik Golinelli <erik@codekraft.it>
2022-10-12 16:42:34 +0200

    fix for metabox that doesn't clear notifications

--- src/scripts/utils/metaBox.js
+++ src/scripts/utils/metaBox.js
@@ -1,28 +1,40 @@
-/**
- * Adding an event listener to the form with the id of `wp-notification-metabox-form` that adds a new notification using the form data
- */
+import { clearNotices } from '../store/reducer';
+import { store } from '../wp-notify';
+
 window.addEventListener('load', () => {
+	/**
+	 * Adding an event listener to the form with the id of `wp-notification-metabox-form` that adds a new notification using the form data
+	 */
 	const wpNotificationMetabox = document.getElementById(
 		'wp-notification-metabox-form'
 	);
 
 	if (wpNotificationMetabox) {
-		document
-			.getElementById('wp-notification-metabox-form')
-			.addEventListener('submit', function (e) {
-				e.preventDefault();
-				const title = document.getElementById(
-					'wp-notification-metabox-form-title'
-				).value;
-				const message = document.getElementById(
-					'wp-notification-metabox-form-message'
-				).value;
+		wpNotificationMetabox.addEventListener('submit', (e) => {
+			e.preventDefault();
+			const title = document.getElementById(
+				'wp-notification-metabox-form-title'
+			).value;
+			const message = document.getElementById(
+				'wp-notification-metabox-form-message'
+			).value;
 
-				wp.notify.add({
-					title,
-					message,
-					location: 'dashboard',
-				});
+			wp.notify.add({
+				title,
+				message,
+				location: 'dashboard',
 			});
+		});
 	}
+
+	/**
+	 *  Adding an event listener to the form with the id of `wp-notification-metabox-form` that handles "clear all notifications"
+	 */
+	const wpNotificationClearAll = document.getElementById(
+		'clear-all-wp-notify'
+	);
+	if (wpNotificationClearAll)
+		wpNotificationClearAll.addEventListener('click', () => {
+			store.dispatch(clearNotices('dashboard'));
+		});
 });
