d68ca9fc30fc0080f5fb39c0847cafe754997b28
Erik Golinelli <erik@codekraft.it>
2023-01-05 16:59:32 +0100

    repo configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,14 +7,4 @@ module.exports = {
 		es2021: true,
 	},
 	extends: ['plugin:@wordpress/eslint-plugin/recommended'],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		requireConfigFile: false,
-		env: { es6: true },
-		ecmaFeatures: {
-			jsx: true,
-			modules: true,
-			experimentalObjectRestSpread: true,
-		},
-	},
 };
