6bf708e7d744d9d059370f00ba936ea1e4447ab5
Erik Golinelli <erik@codekraft.it>
2023-01-06 13:32:30 +0100

    the Component that holds the loop of notices and its wrapper

--- src/scripts/components/NoticesArea.js
+++ src/scripts/components/NoticesArea.js
@@ -1,13 +1,13 @@
-import { comment, Icon } from '@wordpress/icons';
-import { Button } from '@wordpress/components';
-import { dispatch, useSelect } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
 
-import { NOTIFY_NAMESPACE } from '../store/constants';
-import { delay } from '../utils/effects';
+import { defaultContext, NOTIFY_NAMESPACE } from '../store/constants';
+import { NoticeEmpty } from './NoticeEmpty';
+import { NoticeSectionHeader } from './NoticeSectionHeader';
+import { NoticesLoop } from './NoticesLoop';
+import { getSorted } from '../utils/drawer';
 
-import { Notice } from './Notice';
-import { clearNotifyDrawer } from '../utils/drawer';
-import { updateNotice } from '../store/actions';
+export const WEEK_IN_SECONDS = 1000 - 3600 * 24 * 7;
 
 /**
  * WP-Notify toolbar in the secondary position of the admin bar
@@ -17,114 +17,46 @@ import { updateNotice } from '../store/actions';
  * @return {JSX.Element} Notifications
  */
 export const NoticesArea = (props) => {
-	const { splitBy, context } = props;
+	const { splitBy, context = defaultContext } = props;
 
-	const notifications =
-		useSelect(
-			(select) => select(NOTIFY_NAMESPACE).getNotices(context),
-			[]
-		) || [];
+	const notifications = useSelect(
+		(select) => select(NOTIFY_NAMESPACE).getNotices(context),
+		[]
+	);
 
-	/**
-	 * Sets the initial state of the component and binds the updateNoticeState function to the component
-	 */
-	//useEffect(() => {}, []);
+	/** if there aren't notifications fail softly */
+	//if (!notifications) return null;
 
 	/**
-	 * Updating the state of the notifications object.
-	 *
-	 * @param {number}           key
-	 * @param {{status: string}} newProps
+	 * if the context is the adminbar we need to render a list of notifications with the recent notifications and the old notifications
 	 */
-	const updateNoticeState = (key, newProps) => {
-		return updateNotice({
-			key: newProps,
-		});
-	};
+	if (context === defaultContext) {
+		/** Returns the empty notice banner whenever the number of notices is 0 */
+		if (!notifications.length) {
+			return <NoticeEmpty size={96} message={__('empty')} />;
+		}
 
-	/**
-	 * Returns a list of notices
-	 * each notice is a component that has a key, an id, an image, an additional class name, and an onDismiss function
-	 *
-	 * @param {Array} notifyList - An array of objects that contain the notice data.
-	 *
-	 * @return {Array} An array of Notice components.
-	 */
-	const printNotices = (notifyList) => {
-		return notifyList.map((notify, k) => (
-			<Notice
-				{...notify}
-				key={k}
-				id={k}
-				onDismiss={() => {
-					updateNoticeState(k, {
-						status: 'dismissing',
-					});
-					delay(100).then(() =>
-						dispatch('notifyStore').removeNotice(k)
-					);
-				}}
-			/>
-		));
-	};
+		/** split the notifications by date */
+		const sorted = getSorted(notifications, splitBy);
 
-	/**
-	 * It takes a list of notifications, sorts them into two lists,
-	 * one for current notifications and one for past notifications,
-	 * and then renders a list of notifications for each list
-	 *
-	 * @param {Array} notifyList
-	 * @return {Array} A list of notifications split by current (the last 7 days) and past (before current)
-	 */
-	const noticesList = (notifyList) => {
-		// TODO: Component for "Congratulations! You have read all the notifications" (an icon and with text/i18n)
-		if (!notifyList.length)
-			return (
-				<div style={{ padding: '20px', textAlign: 'center' }}>
-					<Icon icon={comment} size={96} />
-					<p>empty</p>
-				</div>
-			);
-
-		// TODO: i've faked the sorting option and whatever argument passed will render a list of notifications split by current (the last 7 days) and past (before current)
-		const sortedNotifications = notifications.reduce(
-			([current, past], item) => {
-				return item.date >= Date.now() / 1000 - 3600 * 24 * 7
-					? [[...current, item], past]
-					: [current, [...past, item]];
-			},
-			[[], []]
+		return (
+			<>
+				{sorted.map((list, index) => (
+					<section key={index}>
+						<NoticeSectionHeader
+							location={location}
+							unreadCount={list.length}
+							isMain={index === 0} // the main section is the first one
+						/>
+						<NoticesLoop notices={list} />
+					</section>
+				))}
+			</>
 		);
+	}
 
-		return sortedNotifications.map((list, index) => (
-			<section key={index}>
-				{!index && list ? (
-					<header>
-						<h2>{list.length} unread notifications</h2>
-						<Button
-							id="clear-all-wp-notify-adminbar"
-							className="wp-notification-action wp-notification-action-markread button-link"
-							onClick={() =>
-								clearNotifyDrawer(this.props.context)
-							}
-						>
-							<span className="ab-icon dashicons-saved"></span>{' '}
-							Mark all as read
-						</Button>
-					</header>
-				) : (
-					<header>
-						<h2>Older notifications</h2>
-					</header>
-				)}
-				{list ? printNotices(list) : null}
-			</section>
-		));
-	};
-
-	if (!notifications) return null;
-
-	return splitBy
-		? noticesList(notifications) || null
-		: printNotices(notifications);
+	/**
+	 * if the context is NOT the adminbar we need to render a simple list of notifications
+	 */
+	return <NoticesLoop notices={notifications} />;
 };
---
+++ src/scripts/components/NoticesLoop.js
@@ -0,0 +1,14 @@
+import { Notice } from './Notice';
+
+/**
+ * Returns a list of notices
+ * each notice is a component that has a key, an id, an image, an additional class name, and an onDismiss function
+ *
+ * @param {Object} prop         -
+ * @param {Array}  prop.notices - An array of objects that contain the notice data.
+ *
+ * @return {Array} An array of Notice components.
+ */
+export const NoticesLoop = ({ notices }) => {
+	return notices.map((notify, index) => <Notice {...notify} key={index} />);
+};
