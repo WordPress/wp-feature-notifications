4e016b2bad62196cb220ebe101c28cc584b21853
Erik Golinelli <erik@codekraft.it>
2023-01-06 12:48:21 +0100

    the two components image and icon were combined

--- src/scripts/components/NoticeImage.js
+++ src/scripts/components/NoticeImage.js
@@ -1,46 +1,43 @@
 import wpLogo from '../../images/WordPressLogo.svg';
 import classNames from 'classnames';
+import { defaultContext } from '../store/constants';
+import { purify } from '../utils/sanitization';
 
 /**
- * Renders an image or icon based on the type of notification
+ * It returns a div with a class name of `wp-notification-image` and `wp-notification-` plus the type of image passed in
+ * an image or a svg element depending on the type of image passed in
  *
- * @param {Object} props
- * @return {JSX} NoticeImage - the image or the icon wrapped into a div
- */
-export const NoticeImage = (props) => {
-	const image =
-		props.image || (props.context === 'adminbar' && wpLogo) || null;
-
-	return (
-		<div
-			className={classNames(
-				'wp-notification-image',
-				'wp-notification-' + props.context
-			)}
-		>
-			<img src={image} alt={''} />
-		</div>
-	);
-};
-
-/**
- * It returns a div with a class name of `wp-notification-image` and `wp-notification-` plus the type of notification,
- * and a background color of the color passed in. Inside the div is a span with a class name of the icon passed in
- * The background color is set to props.color or undefined.
- * The div contains a span with a className of props.icon.
+ * @param {Object} props         - The props object is a JavaScript object that contains all the properties that were passed to the component.
+ * @param {Object} props.icon    - the Object containing the notification icon properties
+ * @param {string} props.context - the location of the notification
  *
- * @param {Object} props - The props object is a JavaScript object that contains all the properties that were passed to the component.
  * @return {JSX.Element} A div with a className of 'wp-notification-image' and 'wp-notification-' + props.type.
  */
-export const NoticeIcon = (props) => (
-	<div
-		className={classNames(
-			'wp-notification-image',
-			'wp-notification-icon',
-			'wp-notification-' + props.context
-		)}
-		style={{ background: props.color || undefined }}
-	>
-		<span className={props.icon}></span>
-	</div>
-);
+export const NoticeIcon = ({ icon, context = defaultContext }) => {
+	/** build the notice container css classes definition */
+	const classes = classNames(
+		'wp-notification-image',
+		!icon?.dashicons || 'wp-notification-icon',
+		'wp-notification-' + (context || 'adminbar')
+	);
+
+	let image;
+
+	// TODO: maybe is better to have a default definition like {type: "svg"} ?
+	if (icon?.svg) {
+		/** Since we don't want to double wrap svg's we need to return the div immediately */
+		return (
+			<div
+				className={classes}
+				dangerouslySetInnerHTML={purify(icon?.svg)}
+			/>
+		);
+	} else if (icon?.dashicons) {
+		image = <span className={'ab-icon dashicons-' + icon.dashicons}></span>;
+	} else {
+		image = <img src={icon?.src || wpLogo} alt={''} />;
+	}
+
+	/** and then finally we can return the image / icon wrapped into a container */
+	return <div className={classes}>{image}</div>;
+};
