e6a15f5784b3b6255cf44723f2fc6a7e70570b8d
Erik Golinelli <erik@codekraft.it>
2023-02-13 17:33:37 +0100

    adding e2e tests

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const jestConfig = {
+	verbose: true,
+	preset: '@wordpress/jest-preset-default',
+	modulePaths: ['<rootDir>'],
+	setupFilesAfterEnv: ['expect-puppeteer'],
+	projects: [
+		{
+			displayName: 'unit',
+			testMatch: ['<rootDir>/tests/jsunit/**/*.test.js'],
+		},
+		{
+			displayName: 'e2e',
+			preset: 'jest-puppeteer',
+			testMatch: ['<rootDir>/tests/jse2e/**/*.test.js'],
+		},
+	],
+};
+
+module.exports = jestConfig;
