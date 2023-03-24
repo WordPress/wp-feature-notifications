54708128c06b87df7c99c65a03e70a502577b241
John Hooks <bitmachina@outlook.com>
2023-03-24 15:27:51 -0700

    feature: add item and collection schemas

--- includes/restapi/class-notification-controller.php
+++ includes/restapi/class-notification-controller.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * REST API controller to get and set notifications.
  *
@@ -30,7 +31,7 @@ class Notification_Controller extends WP_REST_Controller {
 	 *
 	 * @type string
 	 */
-	const NOTIFICATION_BASE = 'notifications';
+	public const NOTIFICATION_BASE = 'notifications';
 
 	public function __construct() {
 		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
@@ -41,27 +42,36 @@ class Notification_Controller extends WP_REST_Controller {
 	 *
 	 * @return void
 	 */
-	public function register_routes() : void {
+	public function register_routes(): void {
 		register_rest_route(
 			self::NAMESPACE,
 			self::NOTIFICATION_BASE,
 			array(
-				'methods'             => WP_REST_Server::READABLE,
-				'args'                => array(),
-				'callback'            => array( $this, 'get_notifications' ),
-				'permission_callback' => array( $this, 'authenticate' ),
+				array(
+					'methods'             => WP_REST_Server::READABLE,
+					'args'                => $this->get_collection_params(),
+					'callback'            => array( $this, 'get_items' ),
+					'permission_callback' => array( $this, 'get_items_permissions_check' ),
+				),
+				'schema' => array( $this, 'get_public_item_schema' ),
 			),
 			false
 		);
 	}
 
 	/**
-	 * Authenticate notification request.
-	 *
-	 * @return boolean true if authenticated, false otherwise.
+	 * Checks if a given request has access to read notifications.
+	 * @param WP_REST_Request $request Full details about the request.
+	 * @return true|WP_Error True if the request has access to view the items, error object otherwise.
 	 */
-	public function authenticate() : bool {
-		// TODO: Implement authentication.
+	public function get_items_permissions_check( $request ) {
+		if ( ! is_user_logged_in() ) {
+			return new WP_Error(
+				'rest_notifications_login_required',
+				__( 'Sorry, you must be logged check notifications.' ),
+				array( 'status' => 401 )
+			);
+		}
 		return true;
 	}
 
@@ -72,7 +82,7 @@ class Notification_Controller extends WP_REST_Controller {
 	 *
 	 * @return WP_REST_RESPONSE|WP_Error REST response or WP Error
 	 */
-	public function get_notifications( $request ) {
+	public function get_items( $request ) {
 		$demo_data = file_get_contents( dirname( __FILE__ ) . '/fake_api.json' );
 
 		if ( empty( $demo_data ) ) {
@@ -80,5 +90,145 @@ class Notification_Controller extends WP_REST_Controller {
 		}
 
 		return rest_ensure_response( json_decode( $demo_data ) );
+	}
+
+	/**
+	 * Retrieves the notification's schema, conforming to JSON Schema.
+	 *
+	 * @return array
+	 */
+	public function get_item_schema() {
+		if ( $this->schema ) {
+			return $this->add_additional_fields_schema( $this->schema );
+		}
+
+		$schema = array(
+			'$schema'    => 'http://json-schema.org/draft-04/schema#',
+			'title'      => 'notification',
+			'type'       => 'object',
+			'properties' => array(
+				'id'           => array(
+					'description' => __( 'Unique identifier for the notification.' ),
+					'type'        => 'integer',
+					'context'     => array( 'view', 'edit', 'embed' ),
+					'readonly'    => true,
+				),
+				'channel_id'   => array(
+					'description' => __( 'Unique identifier for the notification channel.' ),
+					'type'        => 'integer',
+					'context'     => array( 'view', 'edit', 'embed' ),
+					'readonly'    => true,
+				),
+				'created_at'   => array(
+					'description' => __( "The datetime the notification was broadcast, in the site's timezone." ),
+					'type'        => 'string',
+					'format'      => 'date-time',
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+				'dismissed_at' => array(
+					'description' => __( "The datetime the notification was dismissed, in the site's timezone." ),
+					'type'        => 'string',
+					'format'      => 'date-time',
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+				'displayed_at' => array(
+					'description' => __( "The datetime the notification was displayed, in the site's timezone." ),
+					'type'        => 'string',
+					'format'      => 'date-time',
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+				'expires_at'   => array(
+					'description' => __( "The datetime the notification expires, in the site's timezone." ),
+					'type'        => 'string',
+					'format'      => 'date-time',
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+				'priority'     => array(
+					'description' => __( 'The priority of the notification.' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+				'title'        => array(
+					'description' => __( 'The title of the notification.' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+				'message'      => array(
+					'description' => __( 'The message content of the notification.' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+				'meta'         => array(
+					'description' => __( 'The metadata of the notification.' ),
+					'type'        => 'string',
+					'context'     => array( 'view', 'edit', 'embed' ),
+				),
+			),
+		);
+
+		// Cache generated schema on endpoint instance.
+		$this->schema = $schema;
+
+		return $this->add_additional_fields_schema( $this->schema );
+	}
+
+	/**
+	 * Retrieves the query params for collections.
+	 *
+	 *
+	 * @return array Comments collection parameters.
+	 */
+	public function get_collection_params() {
+		$query_params = parent::get_collection_params();
+
+		$query_params['context']['default'] = 'view';
+
+		$query_params['channel'] = array(
+			'default'     => array(),
+			'description' => __( 'Limit result set to notifications assigned to specific channel IDs.' ),
+			'type'        => 'array',
+			'items'       => array(
+				'type' => 'integer',
+			),
+		);
+
+		$query_params['offset'] = array(
+			'description' => __( 'Offset the result set by a specific number of items.' ),
+			'type'        => 'integer',
+		);
+
+		$query_params['order'] = array(
+			'description' => __( 'Order sort attribute ascending or descending. Requires authorization.' ),
+			'type'        => 'string',
+			'default'     => 'desc',
+			'enum'        => array(
+				'asc',
+				'desc',
+			),
+		);
+
+		$query_params['orderby'] = array(
+			'description' => __( 'Sort collection by notifications attribute. Requires authorization.' ),
+			'type'        => 'string',
+			'default'     => 'created_at',
+			'enum'        => array(
+				'created_at',
+				'dismissed_at',
+				'displayed_at',
+			),
+		);
+
+		$query_params['status'] = array(
+			'description' => __( 'Limit result set to notifications assigned a specific status.' ),
+			'default'     => 'undisplayed',
+			'type'        => 'string',
+			'enum'        => array(
+				'undisplayed',
+				'displayed',
+				'dismissed',
+			),
+		);
+
+		return $query_params;
 	}
 }
