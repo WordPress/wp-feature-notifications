9c5b485f92f2cf03dbc2ccaf751cb332e68f7894
John Hooks <bitmachina@outlook.com>
2023-04-03 21:07:29 -0700

    chore: revert .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,3 @@
 // Import the default config file and expose it in the project root.
 // Useful for editor integrations.
-const prettierConfig = require( '@wordpress/prettier-config' );
-
-module.exports = {
-	...prettierConfig,
-  bracketSpacing: true,
-	parenSpacing: true,
-};
+module.exports = require( '@wordpress/prettier-config' );
