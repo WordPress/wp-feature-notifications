1d080b43d3571c75d7ab3f517996c436b06347ca
John Hooks <bitmachina@outlook.com>
2023-04-06 04:30:40 -0700

    test: remove problematic test cases

--- tests/phpunit/tests/test-base-message.php
+++ tests/phpunit/tests/test-base-message.php
@@ -6,8 +6,6 @@ use WP\Notifications\Messages\Base_Message;
 
 class Test_Base_Message extends TestCase {
 
-	const SERIALIZED = 'C:20:"WPNotify_BaseMessage":14:{s:7:"Message";}';
-
 	public function test_it_can_be_instantiated() {
 		$testee = new Base_Message( 'Message' );
 		$this->assertInstanceOf( '\WP\Notifications\Messages\Base_Message', $testee );
@@ -26,20 +24,5 @@ class Test_Base_Message extends TestCase {
 	public function test_it_can_be_cast_to_string() {
 		$testee = new Base_Message( 'Message' );
 		$this->assertEquals( 'Message', (string) $testee );
-	}
-
-	public function test_it_can_be_json_encoded() {
-		$testee = new Base_Message( 'Message' );
-		$this->assertEquals(
-			'"Message"',
-			json_encode( $testee )
-		);
-	}
-
-	public function test_it_can_be_instantiated_from_json() {
-		$json   = '"Message"';
-		$testee = Base_Message::json_unserialize( $json );
-		$this->assertInstanceOf( '\WP\Notifications\Messages\Base_Message', $testee );
-		$this->assertEquals( '\WP\Notifications\Messages\Message', $testee->get_content() );
 	}
 }
--- tests/phpunit/tests/test-base-notification.php
+++ tests/phpunit/tests/test-base-notification.php
@@ -3,13 +3,8 @@
 namespace WP\Notifications\Tests;
 
 use WP\Notifications\Base_Notification;
-use WP\Notifications\Messages\Base_Message;
-use WP\Notifications\Recipients\Recipient_Collection;
-use WP\Notifications\Senders\Base_Sender;
 
 class Test_Base_Notification extends TestCase {
-
-	const JSON_SERIALIZED = '{"Recipient_Collection":[],"Base_Message":"Message","Base_Sender":{"name":"Name 1"}}';
 
 	public function test_it_can_be_instantiated() {
 		$sender_mock     = $this->createMock( '\WP\Notifications\Senders\Base_Sender' );
@@ -45,40 +40,4 @@ class Test_Base_Notification extends TestCase {
 		$this->assertEquals( $recipients_mock, $testee->get_recipients() );
 		$this->assertEquals( $dummy_message, $testee->get_message() );
 	}
-
-	public function test_it_can_be_json_encoded() {
-		$sender_mock      = new Base_Sender( 'Name 1' );
-		$empty_recipients = new Recipient_Collection();
-		$message          = new Base_Message( 'Message' );
-		$testee           = new Base_Notification(
-			$sender_mock,
-			$empty_recipients,
-			$message
-		);
-		$this->assertEquals(
-			self::JSON_SERIALIZED,
-			json_encode( $testee )
-		);
-	}
-
-	// TODO The test below is problematic, the whole concept of serializing a message
-	// which is to be directly initialized as a class is probably not the right direction
-	// and also not how the json api schema would be structured.
-
-	// public function test_it_can_be_instantiated_from_json() {
-	// 	$testee = Base_Notification::json_unserialize( self::JSON_SERIALIZED );
-	// 	$this->assertInstanceOf( '\WP\Notifications\Base_Notification', $testee );
-	// 	$this->assertInstanceOf(
-	// 		'\WP\Notifications\Recipients\Recipient_Collection',
-	// 		$testee->get_recipients()
-	// 	);
-	// 	$this->assertInstanceOf(
-	// 		'\WP\Notifications\Messages\Base_Message',
-	// 		$testee->get_message()
-	// 	);
-	// 	$this->assertInstanceOf(
-	// 		'\WP\Notifications\Senders\Base_Sender',
-	// 		$testee->get_sender()
-	// 	);
-	// }
 }
