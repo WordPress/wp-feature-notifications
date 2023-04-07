1c2f00e9901c24d0a3b314c11d725509f441f7e6
John Hooks <bitmachina@outlook.com>
2023-04-07 15:14:52 -0700

    build: configure eslint for JSDoc types (#249)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,26 @@
  * ESLint presets
  */
 module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: [ './jsconfig.json', 'jsconfig.eslint.json' ],
+	},
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
+	settings: {
+		'import/parsers': {
+			'@typescript-eslint/parser': [ '.js', '.jsx', '.ts', '.tsx' ],
+		},
+		'import/resolver': {
+			typescript: {
+				alwaysTryTypes: true,
+				project: [ './jsconfig.json', 'jsconfig.eslint.json' ],
+			},
+		},
+	},
 	env: {
 		browser: true,
 		es2021: true,
 		jest: true,
 	},
-	extends: ['plugin:@wordpress/eslint-plugin/recommended'],
 };
