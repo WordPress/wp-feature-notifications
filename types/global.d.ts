dafa99f8e43b9894e2ee0bc42a055930d73921f1
John Hooks <bitmachina@outlook.com>
2023-04-08 09:01:14 -0700

    fix: types/global.d.ts

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,11 +1,12 @@
 import { CurriedSelectorsOf } from '@wordpress/data/build-types/types'
-import { NoticeStore, NOTIFY_NAMESPACE } from '../src/scripts/store'
+import { default as store,NoticeStore } from '../src/scripts/store'
+import { NOTIFY_NAMESPACE } from '../src/scripts/store/constants';
 
 declare global {
   interface Window { wp: { notify: any }; wp_notify_data?: { settingsPage: string } }
 }
 
 declare module '@wordpress/data' {
-  function dispatch( key: NOTIFY_NAMESPACE ): typeof import( '../src/scripts/store/actions' );
-  function select( key: NOTIFY_NAMESPACE ): CurriedSelectorsOf< NoticeStore >;
+  function dispatch( key: typeof store | typeof NOTIFY_NAMESPACE ): typeof import( '../src/scripts/store/actions' );
+  function select( key: typeof store | typeof NOTIFY_NAMESPACE ): CurriedSelectorsOf< NoticeStore >;
 }
