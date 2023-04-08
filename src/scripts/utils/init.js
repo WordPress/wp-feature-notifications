4c796bb99d8cbfc3300793f0fb1f4fc2036e3cd0
Erik Golinelli <erik@codekraft.it>
2023-04-08 12:50:42 +0200

    js utils functions updated

---
+++ src/scripts/utils/init.js
@@ -0,0 +1,33 @@
+import { createRoot } from '@wordpress/element';
+import { NoticesArea } from '../components/NoticesArea';
+import Hub from '../components/NotificationHub';
+
+export function addContext( context ) {
+	/** Get the component container */
+	const notifyContainer = document.getElementById(
+		`wp-notification-${ context }`
+	);
+
+	/** Creates a root for NoticesArea component. */
+	const notifyRoot = createRoot( notifyContainer );
+
+	/**
+	 * Renders the component into the specified context
+	 *
+	 * @member {HTMLElement} notifyDash - the area that will host the notifications
+	 */
+	notifyRoot.render( <NoticesArea context={ context } /> );
+}
+
+export function addHub() {
+	/** Get the Notification Hub area (admin bar) */
+	const adminBarWpNotify = document.getElementById(
+		'wp-admin-bar-wp-notification-hub'
+	);
+
+	/** Creates a root for Notification Hub area */
+	const hubRoot = createRoot( adminBarWpNotify );
+
+	/** Init the Notification Hub component */
+	hubRoot.render( <Hub /> );
+}
