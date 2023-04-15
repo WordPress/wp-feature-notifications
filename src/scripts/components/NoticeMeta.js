a6c9b7fe9f6c2f2293b352605741b9b2f04fa4fe
Erik Golinelli <erik@codekraft.it>
2023-04-15 21:10:41 +0200

    Move to @wordpress/escape-html and @wordpress/date packages

--- src/scripts/components/NoticeMeta.js
+++ src/scripts/components/NoticeMeta.js
@@ -1,15 +1,17 @@
-import * as moment from 'moment/moment';
+import { dateI18n } from '@wordpress/date';
 
 /**
  * The notice metadata, for example the source of the notification or the date
  *
  * @param {Object} props
- * @param {number} props.date   The date of the notification.
+ * @param {string} props.date   The date of the notification.
  * @param {string} props.source The source of the notification.
  */
 export const NoticeMeta = ( { date, source } ) => (
 	<p className="wp-notification-meta">
 		<span className="name">{ source }</span> { '\u2022 ' }
-		<span className="date">{ moment( date ).fromNow() }</span>
+		<span className="date">
+			{ dateI18n( 'l jS F Y - h:i A', date, true ) }
+		</span>
 	</p>
 );
