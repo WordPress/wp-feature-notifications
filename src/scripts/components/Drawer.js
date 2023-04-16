9d89e0def961c30b743a1be5d1c26ef25fdbcb93
John Hooks <bitmachina@outlook.com>
2023-04-16 05:56:38 -0700

    feature: resize max width of drawer on window resize

--- src/scripts/components/Drawer.js
+++ src/scripts/components/Drawer.js
@@ -1,4 +1,4 @@
-import { useState } from '@wordpress/element';
+import { useState, useEffect, useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { useShortcut } from '@wordpress/keyboard-shortcuts';
 import { Resizable } from 're-resizable';
@@ -6,11 +6,13 @@ import { Resizable } from 're-resizable';
 import { HUB_WIDTH } from '../constants';
 
 import { NoticesArea } from './NoticesArea';
+
 /**
- * This is a React component that renders a resizable drawer for displaying notifications.
- * The `Drawer` component is being returned, which is an `aside` element containing a `Resizable` component and a `NoticesArea` component.
- * The `Resizable` component allows the user to resize the width of the `Drawer` by dragging its left edge.
- * The `NoticesArea` component displays notifications in the `Drawer`.
+ * A resizable drawer React component for displaying notifications. The returned
+ * `Drawer` component is an `aside` element containing a `Resizable` component and
+ * `NoticesArea` component. The `Resizable` component provides UI controls to resize
+ * the width of the `Drawer` by dragging its left edge. The `NoticesArea` component
+ * displays notifications in the `Drawer`.
  *
  * @param {Object}               props
  * @param {(event: any) => void} props.focus    The `onFocus` event listener for the drawer.
@@ -24,6 +26,29 @@ export const Drawer = ( { focus, blur, instance } ) => {
 	useShortcut( 'wp-feature-notifications/close-drawer', () => blur );
 	const [ width, setWidth ] = useState( /** @type {number} */ ( HUB_WIDTH ) );
 
+	/**
+	 * Maybe resize the maximum width of the drawer if it exceeds the window size.
+	 *
+	 * @return {void}
+	 */
+	const handleWindowResize = useCallback( () => {
+		if ( window.innerWidth < width ) {
+			if ( window.innerWidth < HUB_WIDTH ) {
+				setWidth( HUB_WIDTH );
+			} else {
+				setWidth( window.innerWidth );
+			}
+		}
+	}, [ width ] );
+
+	useEffect( () => {
+		window.addEventListener( 'resize', handleWindowResize );
+
+		return () => {
+			window.removeEventListener( 'resize', handleWindowResize );
+		};
+	}, [ handleWindowResize ] );
+
 	return (
 		<aside
 			id="wp-notifications-hub"
@@ -34,8 +59,13 @@ export const Drawer = ( { focus, blur, instance } ) => {
 			<Resizable
 				size={ { width, height: '100%' } }
 				enable={ { left: true } }
-				onResizeStop={ ( e, direction, ref, d ) => {
-					setWidth( width + d.width );
+				onResizeStop={ ( _e, _direction, _ref, d ) => {
+					const currentWidth = width + d.width;
+					if ( currentWidth > window.innerWidth ) {
+						setWidth( window.innerWidth );
+					} else {
+						setWidth( width + d.width );
+					}
 				} }
 				minWidth={ HUB_WIDTH }
 			>
