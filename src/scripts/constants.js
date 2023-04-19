c070577286a08fab55ceaa63916605719fc11f0e
John Hooks <bitmachina@outlook.com>
2023-04-19 14:17:03 -0700

    fix: rest api namespace (#283)

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -6,7 +6,8 @@ export const STORE_NAMESPACE = /** @type {const} */ 'core/wp-notifications';
 /**
  * API_PATH WP Notification Feature rest api path.
  */
-export const API_PATH = /** @type {const} */ '/wp/v2/notifications/';
+export const API_PATH =
+	/** @type {const} */ '/wp-notifications/v1/notifications/';
 
 /**
  * The width of the notification hub.
