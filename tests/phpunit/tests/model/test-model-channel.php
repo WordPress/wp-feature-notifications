bc0ac29efef680fca2c702f7a8701da54d062b76
John Hooks <bitmachina@outlook.com>
2023-04-21 10:10:35 -0700

    feature(mvp): Stage 2 - add initial model classes

---
+++ tests/phpunit/tests/model/test-model-channel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace WP\Notifications\Test;
+
+use WP_UnitTestCase;
+use WP\Notifications\Model;
+
+class Test_Model_Channel extends WP_UnitTestCase {
+
+	/**
+	 * Test channel model.
+	 */
+	private ?Model\Channel $model = null;
+
+	/**
+	 * Set up each test method.
+	 */
+	public function set_up() {
+		parent::set_up();
+
+		$this->model = new Model\Channel(
+			'core/test',
+			'Test channel',
+			'test',
+			'A test case channel',
+			'WordPress'
+		);
+	}
+
+	/**
+	 * Tear down each test method.
+	 */
+	public function tear_down() {
+		$this->model = null;
+
+		parent::tear_down();
+	}
+
+	/**
+	 * Should be JSON serializable.
+	 */
+	public function test_json_serializable() {
+		$actual   = json_encode( $this->model, JSON_PRETTY_PRINT );
+		$expected = '{
+    "context": "test",
+    "description": "A test case channel",
+    "icon": "WordPress",
+    "name": "core\/test",
+    "title": "Test channel"
+}';
+		$this->assertEquals( $actual, $expected );
+	}
+}
---
+++ tests/phpunit/tests/model/test-model-message.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace WP\Notifications\Test;
+
+use WP_UnitTestCase;
+use WP\Notifications\Model;
+
+class Test_Model_Message extends WP_UnitTestCase {
+
+	/**
+	 * Test message model.
+	 */
+	private ?Model\Message $model = null;
+
+	/**
+	 * Set up each test method.
+	 */
+	public function set_up() {
+		parent::set_up();
+
+		$this->model = new Model\Message(
+			'Testing, testings... 1, 2, 3... testing',
+			'Ok',
+			null,
+			'Test channel',
+			null,
+			'Nope',
+			null,
+			'hammer',
+			null,
+			true,
+			'warning',
+			'Message model test'
+		);
+	}
+
+	/**
+	 * Tear down each test method.
+	 */
+	public function tear_down() {
+		$this->model = null;
+
+		parent::tear_down();
+	}
+
+	/**
+	 * Should be JSON serializable.
+	 */
+	public function test_json_serializable() {
+		$actual   = json_encode( $this->model, JSON_PRETTY_PRINT );
+		$expected = '{
+    "accept_label": "Ok",
+    "channel_title": "Test channel",
+    "dismiss_label": "Nope",
+    "icon": "hammer",
+    "is_dismissible": true,
+    "severity": "warning",
+    "created_at": null,
+    "expires_at": null,
+    "id": null,
+    "message": "Testing, testings... 1, 2, 3... testing",
+    "title": "Message model test"
+}';
+		$this->assertEquals( $actual, $expected );
+	}
+}
