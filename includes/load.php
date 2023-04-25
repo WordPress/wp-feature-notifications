d3511ea9964f8996415c4bdb56770551ceae11bf
John Hooks <bitmachina@outlook.com>
2023-04-25 13:41:30 -0700

    fix: enqueu scripts when a user is logged in

--- includes/load.php
+++ includes/load.php
@@ -7,8 +7,6 @@ use WP\Notifications\REST;
 
 /**
  * Activation hook function of the WP Notification plugin.
- *
- * @return void
  */
 function activation_hook() {
 	require_once WP_FEATURE_NOTIFICATION_PLUGIN_DIR . '/includes/class-activator.php';
@@ -19,8 +17,6 @@ register_activation_hook( __FILE__, '\WP\Notifications\activation_hook' );
 
 /**
  * Uninstall hook function of the WP Notification plugin.
- *
- * @return void
  */
 function uninstall_hook() {
 	require_once WP_FEATURE_NOTIFICATION_PLUGIN_DIR . '/includes/class-uninstaller.php';
@@ -31,8 +27,6 @@ register_uninstall_hook( __FILE__, '\WP\Notifications\uninstall_hook' );
 
 /**
  * REST API initialization hook of the WP Notification plugin.
- *
- * @return void
  */
 function register_routes() {
 	$channel_controller      = new REST\Channel_Controller();
@@ -53,7 +47,7 @@ add_action( 'rest_api_init', '\WP\Notifications\register_routes' );
  * @param WP_Admin_Bar $wp_admin_bar Toolbar instance.
  */
 function admin_bar_item( WP_Admin_Bar $wp_admin_bar ) {
-	if ( ! is_admin() ) {
+	if ( ! is_user_logged_in() ) {
 		return;
 	}
 
@@ -67,12 +61,17 @@ function admin_bar_item( WP_Admin_Bar $wp_admin_bar ) {
 	);
 	$wp_admin_bar->add_node( $args );
 }
+
 add_action( 'admin_bar_menu', '\WP\Notifications\admin_bar_item', 1 );
 
 /**
  * Register and enqueue a wp-notifications scripts and stylesheet in WordPress admin.
  */
 function enqueue_admin_assets() {
+	if ( ! is_user_logged_in() ) {
+		return;
+	}
+
 	/* Load styles */
 	wp_register_style( 'wp_notifications', WP_FEATURE_NOTIFICATION_PLUGIN_DIR_URL . '/build/wp-notifications.css', array(), WP_FEATURE_NOTIFICATION_PLUGIN_VERSION );
 	wp_enqueue_style( 'wp_notifications' );
@@ -83,4 +82,6 @@ function enqueue_admin_assets() {
 	wp_enqueue_script( 'wp_notifications' );
 }
 
+
+add_action( 'wp_enqueue_scripts', '\WP\Notifications\enqueue_admin_assets', 0 );
 add_action( 'admin_enqueue_scripts', '\WP\Notifications\enqueue_admin_assets', 0 );
