09b92b2c1a5522ef2e6a5d38535760bc63c88fbd
Erik Golinelli <erik@codekraft.it>
2023-07-23 21:05:14 +0200

    updates the storybook configuration in order to support typescript (#392)
    + deps updated to storybook 7.1.0

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,18 +1,18 @@
 // automatically get all the stories from the stories folder
 const doc = ['../stories/**/*.@(mdx)'].filter(Boolean);
 const stories = ['../stories/**/*.@(js|jsx|ts|tsx)'].filter(Boolean);
-
 module.exports = {
   stories: [...doc, ...stories],
 	addons: [
 		'@storybook/addon-a11y',
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-interactions'
+		'@storybook/addon-interactions',
+		'@storybook/addon-mdx-gfm'
 	],
 	framework: {
 		name: '@storybook/react-webpack5',
-		options: {},
+		options: { lazyCompilation: true }
 	},
 	features: {
 		babelModeV7: true,
@@ -20,13 +20,31 @@ module.exports = {
 		storyStoreV7: true,
 	},
 	webpackFinal: async (config) => {
+		// Add the ts loader
+		config.module.rules.push(
+			{
+				test: /\.tsx?$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ["@wordpress/babel-preset-default"],
+						},
+					},
+				],
+				exclude: /node_modules/
+			},
+		);
+
 		// Add the scss loader
 		config.module.rules.push({
 			test: /\.scss$/,
 			use: ['style-loader', 'css-loader', 'sass-loader'],
 		});
 
-		// Return the altered config
 		return config;
 	},
+	docs: {
+		autodocs: true
+	}
 };
